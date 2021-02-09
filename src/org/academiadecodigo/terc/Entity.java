37384dc42d210df04987acd7aca31e3ade3472ab
Mariana <mariana.barros@academiadecodigo.org>
2021-02-09 19:22:25 -0100

    add ECS entity add .gitignore changes

---
+++ src/org/academiadecodigo/terc/Entity.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.terc;
+
+public interface Entity {
+    /*
+    * What happens on initialization
+     */
+    void start();
+    /*
+     * What happens on every frame
+     */
+    void update();
+}
